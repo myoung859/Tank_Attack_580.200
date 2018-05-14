4adbe37c9768dded91d2a8e5897ab742ee3489a2
myoung859 <myoung859@gmail.com>
2018-05-14 19:31:47 -0400

    Add files via upload
    Un-deleted the options

--- helpers.py
+++ helpers.py
@@ -6,39 +6,38 @@ Created on Sun May 13 17:35:38 2018
 """
 import pygame
 from math import radians,sin,cos
+import csv
+
+def options_prompt(filename, x_dim, y_dim, gravity, drag,wind_max):
+
+    filew = open(filename, 'w',newline = '')
+    output = csv.writer(filew)
+    output.writerow([int(input("Please input the horizontal window size (Current value is "+ str(x_dim) +"): "))])
+    output.writerow([int(input("Please input the vertical window size (Current value is "+ str(y_dim) +"): "))])
+    output.writerow([float(input("Please input the gravity strength (Current value is "+ str(gravity) +"): "))])
+    output.writerow([float(input("Please input the drag constant (Current value is "+ str(drag) +"): "))])
+    output.writerow([float(input("Please input the maximum wind speec (Current value is "+ str(drag) +"): "))])
 
 class Tank(pygame.sprite.Sprite):
-    def __init__(self, pos_x, x_dim, y_dim, player, img):
+    def __init__(self, pos_x, x_dim, y_dim, color, player):
         super().__init__()
-        self.image = pygame.image.load(img)
+        self.color = color
+        self.image = pygame.Surface((25, 13))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = (pos_x, y_dim-63)
-        self.posx = pos_x
         self.player = player
         self.x_max = x_dim
     def move(self):
-        dist = 516
+        dist = 666 #Hail santa!
         while (dist > 50 or dist <= -50):
-            dist = int(input("Please enter the distance (positive-RIGHT or negative-LEFT) to move, up to 50 meters: "))
-        self.posx = self.posx + (dist) #Inspired by https://bit.ly/2KkNOp8
-        if (self.posx <= 20):
-            self.posx = 20
-            print("You can't get out of this one.")
-        if (self.posx >= self.x_max - 20):
-            self.posx = self.xmax - 20
-            print("You can't get out of this one.")
-        return self.posx
-
-    def showtank(self):
-       pic = self.image
-       return pic         
-
-    def position(self):
-        return self.posx
-
-    def color(self):
-        return self.color
-    
+            dist = int(input("Please enter the distance (positive or negative) to move, up to 50 meters: "))
+        self.rect.left = self.rect.x + (5* dist) #Inspired by https://bit.ly/2KkNOp8
+        if (self.rect.left < 0):
+            self.rect.left = 0
+        if (self.rect.right > self.x_max):
+            self.rect.right = self.x_max
+            
     def fire(self):
         None
 class Turret(pygame.sprite.Sprite):
